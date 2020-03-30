b176b476fc53d29464ccb04b5587760932f66d5c
isubas <irfan.subas@uzem.omu.edu.tr>
2020-03-30 12:11:40 +0300

    SSS için arama özelliği ekle

---
+++ js/search_faqs.js
@@ -0,0 +1,88 @@
+(function() {
+  function displaySearchResults(results, store) {
+    var searchResults = document.getElementById('search-results');
+
+    template = function (question, answer) {
+      return `
+        <div class="box">
+          <article class="media">
+            <div class="media-content">
+              <h5 class="title is-5 has-text-info">${question}</h5>
+              <hr>
+                <div class="content">
+                  <p>${answer}</p>
+                </div>
+            </div>
+          </article>
+        </div>
+      `
+    }
+
+    if (results.length) {
+      var elements = [];
+
+      for (var i = 0; i < results.length; i++) {
+        var item = store[results[i].ref];
+        elements.push(template(item.question, item.answer))
+      }
+
+      searchResults.innerHTML = elements.join('');
+    }
+    else {
+      searchResults.innerHTML = `
+        <div class="notification is-danger is-light">
+          Her hangi bir cevap bulunamadı!
+        </div>
+      `
+    }
+  }
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+
+      if (pair[0] === variable) {
+          return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+      }
+    }
+  }
+
+  var searchTerm = getQueryVariable('query');
+
+  if (searchTerm) {
+    document.getElementById('search-box').setAttribute("value", searchTerm);
+
+    // Initalize lunr with the fields it will be searching on. I've given title
+    // a boost of 10 to indicate matches on this field are more important.
+    window.idx = lunr(function () {
+      this.field('id');
+      this.field('question');
+      this.field('answer');
+
+      for (var key in window.store) {
+        this.add(
+          {
+            'id': key,
+            'question': window.store[key].question,
+            'answer': window.store[key].answer
+          }
+        )
+      }
+    });
+
+    var results = idx.search(searchTerm); // Get lunr to perform a search
+    displaySearchResults(results, window.store); // We'll write this in the next section
+  }
+  else {
+    var results = []
+    for (var key in window.store) {
+      results.push({
+        ref: key
+      })
+    }
+    displaySearchResults(results, window.store)
+  }
+})();
